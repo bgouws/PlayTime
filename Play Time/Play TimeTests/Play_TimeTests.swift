46155c771c983256a6041cdff639a4d2119b847a
Brandon Gouws <bgouws@DVTL101265.local>
2020-02-12 09:09:23 +0200

    Added swiftlint

--- Play Time/Play TimeTests/Play_TimeTests.swift
+++ Play Time/Play TimeTests/Play_TimeTests.swift
@@ -8,9 +8,9 @@
 
 import XCTest
 @testable import Play_Time
-
+//swiftlint:disable all
 class Play_TimeTests: XCTestCase {
-
+//swiftlint:enable all
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
