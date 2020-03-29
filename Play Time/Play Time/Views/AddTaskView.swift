316879f7b714cd2a15b1bd3b86a65ba7eff04dd6
Brandon Gouws <60609865+bgouws@users.noreply.github.com>
2020-03-29 23:40:28 +0200

    Multiple ui updates (#42)

---
+++ Play Time/Play Time/Views/AddTaskView.swift
@@ -0,0 +1,106 @@
+//
+//  AddTaskView.swift
+//  Play Time
+//
+//  Created by Brandon Gouws on 2020/03/29.
+//  Copyright © 2020 Brandon Gouws. All rights reserved.
+//
+
+import UIKit
+import PTFramework
+
+class AddTaskView: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
+    let hour = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"]
+    let minute = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+                "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+    let seconds = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                   "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                   "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                   "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+                   "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+                   "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+    var hourSelected = ""
+    var minuteSelected = ""
+    var secondSelected = ""
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 6
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return hour.count
+        } else if component == 2 {
+            return minute.count
+        } else if component == 4 {
+            return seconds.count
+        }
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return hour[row]
+        case 1:
+            return "H"
+        case 2:
+            return minute[row]
+        case 3:
+            return "M"
+        case 4:
+            return seconds[row]
+        case 5:
+            return "S"
+        default:
+            return "Error"
+        }
+    }
+    @IBAction func btnCreateTask(_ sender: Any) {
+        print("++++++++")
+        print("Hour: \(hourSelected) Minute: \(minuteSelected) Second: \(secondSelected)")
+        print("++++++++")
+        actIndicator.isHidden = false
+        let myAddTask = TasksViewModel()
+        myAddTask.insertTask(taskTitle: txtTaskTitle.text!, hour: hourSelected, minute: minuteSelected,
+                             second: secondSelected) { (success) in
+            if success {
+                actIndicator.isHidden = true
+            } else {
+                actIndicator.isHidden = true
+                //call alert
+            }
+        }
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            print("Hour: \(hour[row])")
+            hourSelected = hour[row]
+        } else if component == 2 {
+            print("Minute: \(minute[row])")
+            minuteSelected = minute[row]
+        } else if component == 4 {
+            print("Second: \(seconds[row])")
+            secondSelected = seconds[row]
+        }
+    }
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var btnCreateTask: UIButton!
+    @IBOutlet weak var txtTaskTitle: UITextField!
+    @IBOutlet weak var actIndicator: UIActivityIndicatorView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerView.delegate = self
+        btnCreateTask.defaultButton()
+        txtTaskTitle.customTextBox()
+        txtTaskTitle.delegate = self
+        actIndicator.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+}
--- Play Time/Play Time/Views/TaskListViewController.swift
+++ Play Time/Play Time/Views/TaskListViewController.swift
@@ -16,31 +16,22 @@ class TaskListViewController: UIViewController {
     var finalMinute = ""
     var finalSecond = ""
     var finalTitle = ""
-    let move = "SignOutView"
     var flag = false
-    //@IBOutlet weak var btnAddTask: UIButton!
-    @IBOutlet weak var btnLogout: UIButton!
-    @IBOutlet weak var btnCancel: UIButton!
-    @IBOutlet weak var txtTaskTitle: UITextField!
-    @IBOutlet weak var txtTitle: UITextField!
-    @IBOutlet weak var txtDuration: UITextField!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var btnAddSingleTask: UIButton!
     @IBOutlet weak var btnProfile: UIButton!
-    //@IBOutlet weak var btnAddNow: UIButton!
     var tasks: [PTTask] = []
+    let getTasks = TasksViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Calling Api")
-        //PTApiCall.ptPreparePlayList()
         let myPTPlayMusic = PTPlayMusic()
         myPTPlayMusic.readData()
-        tasks = createTask()
+        tableView.reloadData()
         tableView.delegate = self
         tableView.dataSource = self
         authenticateUserAndConfigure()
         btnProfile.profileButton()
-        // Do any additional setup after loading the view.
     }
     func authenticateUserAndConfigure() {
         if Auth.auth().currentUser == nil {
@@ -49,89 +40,21 @@ class TaskListViewController: UIViewController {
             }
         }
     }
-    @IBAction func btnLogout(_ sender: Any) {
-        let alertController = UIAlertController(title: nil, message: "Are you sure you want to sign out?",
-                                                preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "Sign Out", style: .destructive,
-                                                handler: {(_) in self.signOut(moveTo: self.move)
-        }))
-        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
-    func createTask() -> [PTTask] { //Read text file here
-        //Test
-            //Creating textfile test
-            let fileName = "Tasks"
-            // swiftlint:disable all
-            let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask,
-                                                              appropriateFor: nil, create: true)
-            // swiftlint:enable all
-            let fileURL = DocumentDirURL.appendingPathComponent(fileName).appendingPathExtension("txt")
-            print("File Path: \(fileURL.path)")
-            let writeString = "This is the data that I am writing the the text file"
-            do {
-                //Write to file
-                try writeString.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
-            } catch let error as NSError {
-                print("Failed to write to URL ")
-                print(error)
-            }
-            var readString = ""
-            do {
-                readString = try String(contentsOf: fileURL)
-            } catch let error as NSError {
-                print("Failed to read file")
-                print(error)
-            }
-            print("Contents of the file: \(readString)")
-        //Test
-        var tempTask: [PTTask] = []
-        let task1 = PTTask(title: "Cooking", hour: "01", minute: "00", second: "00")
-        let task2 = PTTask(title: "Cleaning", hour: "00", minute: "30", second: "00")
-        let task3 = PTTask(title: "Working", hour: "02", minute: "30", second: "00")
-        let task4 = PTTask(title: "Gyming", hour: "00", minute: "00", second: "10")
-        let task5 = PTTask(title: "Shopping", hour: "00", minute: "45", second: "45")
-        tempTask.append(task1)
-        tempTask.append(task2)
-        tempTask.append(task3)
-        tempTask.append(task4)
-        tempTask.append(task5)
-        print("Music taste successfully saved - PTFramework")
-        return tempTask
-    }
-    func insetNewTask() {
-        let duration = txtDuration.text?.components(separatedBy: ":")
-        finalHour = (duration?[0])!
-        finalMinute = (duration?[1])!
-        finalSecond = (duration?[2])!
-        finalTitle = txtTitle.text!
-        let newTask = PTTask(title: finalTitle, hour: finalHour, minute: finalMinute, second: finalSecond)
-        tasks.append(newTask)
-        let indexPath = IndexPath(row: tasks.count - 1, section: 0)
-        tableView.beginUpdates()
-        tableView.insertRows(at: [indexPath], with: .fade)
-        tableView.endUpdates()
-        txtTitle.text = ""
-        txtDuration.text = ""
-        view.endEditing(true)
+    override func viewWillAppear(_ animated: Bool) {
+        let myData = TasksViewModel()
+        myData.getListOfTasks { (listOfTasks) in
+            self.tasks = listOfTasks
+            print("\(self.tasks[0].title) \(self.tasks[0].minute) \(self.tasks[0].second)")
+            self.tableView.reloadData()
+        }
     }
     @IBAction func btnAddSingleTask(_ sender: Any) {
-        insetNewTask()
+        self.performSegue(withIdentifier: "ToCreateTask", sender: self)
     }
     @IBAction func btnProfileTapped(_ sender: Any) {
         self.performSegue(withIdentifier: "ToProfileView", sender: self)
     }
-    func signOut(moveTo: String) {
-        do {
-            flag = false
-            try Auth.auth().signOut()
-            self.performSegue(withIdentifier: "NotLoggedInView", sender: self)
-        } catch let error {
-            print("Failed to sign out with error: ", error.localizedDescription)
-        }
-    }
 }
-
 extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
@@ -144,7 +67,6 @@ extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
         cell.setTask(task: task)
         return cell
     }
-    //To enable delete
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
